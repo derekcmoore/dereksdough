3565408c65c1ff18818fe9f39c9aa2c5cce6fdeb
Derek Moore <moorder1112@gmail.com>
2024-05-05 23:17:40 -0400

    show active page in nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Nav.scss";
+import { useNavigate } from "react-router-dom";
 import useWindowDimensions from "../hooks/useWindowDimensions";
 
 import logo from "../assets/logos/full.png";
@@ -8,6 +9,12 @@ import profile from "../assets/icons/profile.svg";
 function Nav() {
   const { width } = useWindowDimensions();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState(location.pathname);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    setCurrentPage(location.pathname);
+  }, [navigate]);
 
   if (width > 768) isMenuOpen && setIsMenuOpen(false);
 
@@ -25,23 +32,25 @@ function Nav() {
           <span></span>
         </div>
         <div className="logo-wrapper">
-          <img src={logo} className="logo" alt="Derek's Dough Logo" />
+          <a href="/">
+            <img src={logo} className="logo" alt="Derek's Dough Logo" />
+          </a>
         </div>
 
         <div className={`items ${isMenuOpen ? "show" : "hide"}`}>
-          <div className="item">
+          <div className={`item ${currentPage == "/" ? "active" : ""}`}>
             <a href="/">Home</a>
           </div>
 
-          <div className="item">
+          <div className={`item ${currentPage == "/about" ? "active" : ""}`}>
             <a href="about">About</a>
           </div>
 
-          <div className="item">
-            <a href="menu">Menu</a>
+          <div className={`item ${currentPage == "/recipes" ? "active" : ""}`}>
+            <a href="recipes">Recipes</a>
           </div>
 
-          <div className="item">
+          <div className={`item ${currentPage == "/contact" ? "active" : ""}`}>
             <a href="contact">Contact</a>
           </div>
         </div>
